f0783d126685a51b34ad2fab0a89f1e865e98207
Daiki Anjiki <daikianjiki@gmail.com>
2023-10-12 13:31:10 -0700

    practicing with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import UseRefForm from './components/forms/useRefForm';
 import UseStateForm from './components/forms/useStateForm';
 import UseCustomHookForm from './components/forms/useCustomHookForm';
 import FetchDataUsingHooks from './components/fetch/fetchDataUsingHooks';
 import FetchDataUsingGraphQL from './components/fetch/fetchDataUsingGraphQL';
+import { CgHello } from 'react-icons/cg'
+import { BiCalendar } from 'react-icons/bi'
+import Search from './components/Search';
+import AddAppointment from './components/AddAppointment';
 
 function App() {
   return (
     <>
-      <UseRefForm />
+      {/* <UseRefForm />
       <UseStateForm />
       <UseCustomHookForm />
 
       <FetchDataUsingHooks />
-      <FetchDataUsingGraphQL />
+      <FetchDataUsingGraphQL /> */}
+      {/* <h1 className="text-3xl font-bold underline">
+        <CgHello /> Hello world!
+      </h1> */}
+      <div className='App container mx-auto mt-3 font-thin'>
+        <h1 className='text-5xl mb-3'>
+          <BiCalendar className='inline-block text-red-400 align-top' />Your Appointments</h1>
+          <AddAppointment />
+          <Search />
+      </div>
     </>
   )
 }
